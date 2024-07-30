de277283ce55201d85a9f30d37dad58442b8231f
GioCG <gcarrera-2020313@kinal.edu.gt>
2024-07-30 10:13:32 -0600

    initial commit

---
+++ src/main/java/com/giovannicarrera/primeraapp/PrimeraappApplication.java
@@ -0,0 +1,13 @@
+package com.giovannicarrera.primeraapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimeraappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrimeraappApplication.class, args);
+	}
+
+}
